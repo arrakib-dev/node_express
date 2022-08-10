7a9f7bc9e3dce488261df71416febb32c8f6801d
Abdur Rahman <abdur.rahman@tuhh.de>
2022-08-10 19:09:16 +0200

    section 5

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // include express js
 const express = require('express')
 
+
+
+// path module
+const path = require('path')
+
+
+//include admin routes
+const adminRoutes = require('./routes/admin')
+// include shopRoutes
+const shopRoutes = require('./routes/shop')
+
 //initiate express
 const app = express()
 
-app.use('/user', (req, res, next)=>{
-    console.log('user route')
-    res.send('<h1>Hello From user</h1>')
-})
+//static file server
 
-app.use('/', (req, res, next)=>{
-    console.log('root route')
-    res.send('<h1>Hello From root</h1>')
+app.use(express.static(path.join(__dirname, 'public')))
+
+// routes
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+
+// 404
+app.use((req, res)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 //listen to 300 port on localhost
